80bbb6e91508e64539a77d61bda5e32b9843bb3d
Shermam Miranda <shermam.miranda@goodeggs.com>
2022-02-03 13:38:59 -0300

    Added qr code functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import express from "express";
+import QRCode from "qrcode";
+import { promisify } from "util";
+
+const toDataURL = promisify(QRCode.toDataURL);
 
 const app = express();
 
 app.use(express.static("src/web"));
+
+app.get("/:name", async (req, res) => {
+  const { name } = req.params;
+  const url = await toDataURL(`http://localhost/confirmation/${name}`);
+  res.type("text/html");
+  res.send(
+    `<html><body><img src="${url}" height="500" width="500"/></body></html>`
+  );
+});
+
+app.get("/confirmation/:name", async (req, res) => {
+  const { name } = req.params;
+
+  res.send(`Bem vinda a festa da Anna Lara ${name}!`);
+});
 
 app.listen(8888);
